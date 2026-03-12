3e4f8c472be1762f8f42615d1cb29a9f3f5d1f57
szymek2013pl <szymonstraszak06@gmail.com>
2026-03-12 11:43:11 +0100

    pierwszy etap

---
+++ dynamic_dict.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+
+    std::string phrase;
+
+    std::getline(std::cin, phrase);
+
+    std::istringstream new_stream (phrase);
+
+    std::string pol_word;
+    std::string eng_word;
+
+    std::getline(new_stream, pol_word, ':');
+    std::getline(new_stream, eng_word);
+
+    std::cout << "slowo: " << pol_word << ", tlumaczenie: " << eng_word << std::endl;
+
+}
