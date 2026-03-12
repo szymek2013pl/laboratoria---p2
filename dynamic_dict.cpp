8bff9181157fe5d94ddc6e07b02269e3b2545f03
szymek2013pl <szymonstraszak06@gmail.com>
2026-03-12 11:54:26 +0100

    po francusku

--- dynamic_dict.cpp
+++ dynamic_dict.cpp
@@ -11,11 +11,11 @@ int main() {
     std::istringstream new_stream (phrase);
 
     std::string pol_word;
-    std::string eng_word;
+    std::string fr_word;
 
     std::getline(new_stream, pol_word, ':');
-    std::getline(new_stream, eng_word);
+    std::getline(new_stream, fr_word);
 
-    std::cout << "slowo: " << pol_word << ", tlumaczenie: " << eng_word << std::endl;
+    std::cout << "slowo: " << pol_word << ", tlumaczenie: " << fr_word << std::endl;
 
 }
