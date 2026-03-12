4a2f1671b707b0cf32b374198c0bbcc68939bd4a
szymek2013pl <szymonstraszak06@gmail.com>
2026-03-12 21:41:12 +0100

    powtorka

--- pager.cpp
+++ pager.cpp
@@ -1,47 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-struct bounded_buffer {
-
-    std::vector <std::string> buffer;
-    int capacity;
-
-};
-
-int buffer_size(const bounded_buffer * buffer) {
-
-    return buffer -> buffer.size();
-
-}
-
-int buffer_capacity(const bounded_buffer * buffer) {
-
-    return buffer -> capacity;
-
-}
-
-void flush_buffer(bounded_buffer * buffer) {
-
-    for (int i = 0; i < buffer -> buffer.size(); i++) {
-
-        std::cout << buffer->buffer[i] << std::endl;
-
-    }
-
-}
-
-void add_to_buffer(bounded_buffer * buffer, std::string line) {
-
-    if (buffer->buffer.size() >= buffer->capacity) {
-
-        return;
-
-    }
-
-    buffer->buffer.push_back(line);
-
-}
+#include "bounded_buffer.hpp"
 
 int main () {
 
